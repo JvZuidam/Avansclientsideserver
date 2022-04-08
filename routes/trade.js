aba6994e3ecb027f9cbb3d7aec4795010faab0cd
Jim van Zuidam <jimvanzuidam@gmail.com>
2022-04-08 21:39:30 +0200

    Add Trade routes

--- routes/trade.js
+++ routes/trade.js
@@ -24,7 +24,6 @@ router.post("/new", (request, result) => {
     console.log("Create a new trade");
     const firstItem = "";
     const secondItem = "";
-    const nothingValue = "";
 
     const traderId = request.body.traderId;
     const tradeeId = request.body.tradeeId;
@@ -64,9 +63,11 @@ router.post("/new", (request, result) => {
 
 
 //Get all trades
-router.get("", (request, result) => {
+router.get("/:userid", (request, result) => {
     console.log("Get all trades aangeroepen");
-    Trade.find({}, function (err, docs) {
+    const userId = request.params.userid;
+
+    Trade.find({trader: userId}, function (err, docs) {
         if (err || docs === null) {
             responseMessages.ErrorCode412(result);
         } else {
@@ -76,11 +77,12 @@ router.get("", (request, result) => {
 });
 
 //Get a trade by id
-router.get("", (request, result) => {
+router.get("/:userid/:id", (request, result) => {
     console.log("Get trade by id aangeroepen");
     const tradeId = request.params.id;
+    const userId = request.params.userid;
 
-    Trade.find({_id: tradeId}, function (err, docs) {
+    Trade.find({_id: tradeId, trader: userId}, function (err, docs) {
         if (err || docs === null) {
             responseMessages.ErrorCode412(result);
         } else {
@@ -90,13 +92,53 @@ router.get("", (request, result) => {
 });
 
 //Update a trade
+router.put("/:userid/:id", (request, result) => {
+    console.log("Update trade aangeroepen");
+    const userId = request.params.userid;
+    const tradeId = request.params.id;
+    const deckIdToTrade = request.body.deckToTrade;
+    const deckIdToReceive = request.body.deckToReceive;
+    const cardIdToTrade = request.body.cardToTrade;
+    const cardIdToReceive = request.body.cardToReceive;
+
+    Trade.find({_id: tradeId, trader: userId}, function (err, docs) {
+        if (err || docs === null) {
+            responseMessages.ErrorCode412(result);
+        } else {
+            if (deckIdToTrade == null && deckIdToReceive == null) {
+                console.log("inside trade deck");
+                Trade.updateOne({_id: tradeId}, {$set: {cardToTrade: cardIdToTrade, cardToReceive: cardIdToReceive}}, function(err, docs) {
+                    if (err || docs == null) {
+                        responseMessages.ErrorCode500(result)
+                    } else {
+                        responseMessages.SuccessCode200UpdateTrade(result)
+                    }
+                })
+            } else if (cardIdToTrade == null && cardIdToReceive == null) {
+                console.log("inside trade card");
+                console.log(deckIdToTrade);
+                console.log(deckIdToReceive);
+                Trade.updateOne({_id: tradeId}, {$set: {deckToTrade: deckIdToTrade, deckToReceive: deckIdToReceive}}, function(err, docs) {
+                    if (err || docs == null) {
+                        responseMessages.ErrorCode500(result)
+                    } else {
+                        responseMessages.SuccessCode200UpdateTrade(result)
+                    }
+                })
+            } else {
+                responseMessages.ErrorCode500(result)
+            }
+        }
+    })
+})
 
 //Delete a trade
-router.delete("/:id", (request, result) => {
+router.delete("/:userid/:id", (request, result) => {
     console.log("Delete trade aangeroepen");
     const tradeId = request.params.id;
+    const userId = request.params.userid;
 
-    Trade.deleteOne({_id: tradeId}, function (err, docs) {
+    Trade.deleteOne({_id: tradeId, trader: userId}, function (err, docs) {
         if (err || docs === null) {
             responseMessages.ErrorCode412(result);
         } else {
